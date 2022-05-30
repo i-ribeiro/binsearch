38bf5f56bcd7f92968dcee099c3fca342a972419
Isaac Ribeiro <i.ribeiro@outlook.com>
2022-05-30 15:51:29 -0400

    Initial commit

---
+++ src/Binarysearch.java
@@ -0,0 +1,13 @@
+/**
+ * Recursive and non-recursive binary search algorithms.
+ * Helper method to generate sorted random integer arrays included. 
+ * @author Isaac Ribeiro, James Mwangi
+ * Student Number: 040957075
+ * Course: CST8130 - Data Structures
+ * Professor: James Mwangi PhD. 
+ * CET-CS-Level 3
+ */
+public class Binarysearch {
+	
+	
+}
---
+++ src/Lab3binsearchTest.java
@@ -0,0 +1,15 @@
+/**
+ * Demonstrates and tests the Binarysearch class interactively. 
+ * @author Isaac Ribeiro, James Mwangi
+ * Student Number: 040957075
+ * Course: CST8130 - Data Structures
+ * Professor: James Mwangi PhD. 
+ * CET-CS-Level 3
+ */
+public class Lab3binsearchTest {
+
+	public static void main(String[] args) {
+		
+		
+	}
+}
